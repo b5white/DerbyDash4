6699c5a2e2ccbaa34985ca368486c7d65be18236
b5white <b55white@gmail.com>
2025-04-04 00:41:19 -0500

    Added ImageID

--- DerbyDash/Services/RaceService.cs
+++ DerbyDash/Services/RaceService.cs
@@ -14,12 +14,12 @@ namespace DerbyDash.Services {
 
         public RaceComponents CreateTrack(string problemSetIdentifier) {
             List<Car> Cars = [
-                new Car { index = 0, ImageUrl = "Racecar1.png", Top = 9999 }, // Initialize with off-screen position
-                new Car { index = 1, ImageUrl = "Racecar2.png", Top = 9999 },
-                new Car { index = 2, ImageUrl = "Racecar3.png", Top = 9999 },
-                new Car { index = 3, ImageUrl = "Racecar4.png", Top = 9999 },
-                new Car { index = 4, ImageUrl = "Racecar5.png", Top = 9999 },
-                new Car { index = 5, ImageUrl = "Racecar6.png", Top = 9999 }
+                new Car { index = 0, ImageId = 1, Top = 9999 }, // Initialize with off-screen position
+                new Car { index = 1, ImageId = 2, Top = 9999 },
+                new Car { index = 2, ImageId = 3, Top = 9999 },
+                new Car { index = 3, ImageId = 4, Top = 9999 },
+                new Car { index = 4, ImageId = 5, Top = 9999 },
+                new Car { index = 5, ImageId = 6, Top = 9999 }
             ];
 
             for (int i = 1; i < Cars.Count; i++) {
