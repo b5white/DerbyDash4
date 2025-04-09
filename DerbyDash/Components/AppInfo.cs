fd7df9723b7cb8aca5a69ea93b17a8bf1cbbfac8
b5white <b55white@gmail.com>
2025-04-09 12:01:13 -0500

    Formatting

--- DerbyDash/Components/AppInfo.cs
+++ DerbyDash/Components/AppInfo.cs
@@ -3,7 +3,7 @@
 namespace DerbyDash.Components {
 
     public static class AppInfo {
-        static string BuildNo = "0281";
+        static string BuildNo = "0282";
  
         public static string Version = GitVersionInformation.Major + "." + GitVersionInformation.Minor + "." + GitVersionInformation.BuildMetaData + "." + BuildNo;
     }
