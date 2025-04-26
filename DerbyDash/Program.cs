0f13ab362aa3b4bdc1993176fb5ce6085621d2dc
felipedev418 <storm712418@gmail.com>
2025-04-26 22:05:39 +0900

    update auth pages

--- DerbyDash/Program.cs
+++ DerbyDash/Program.cs
@@ -3,6 +3,7 @@ using DerbyDash.Components.Account;
 using DerbyDash.Data;
 using DerbyDash.Services;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,6 +22,35 @@ namespace DerbyDash {
             builder.Services.AddScoped<IdentityUserAccessor>();
             builder.Services.AddScoped<IdentityRedirectManager>();
             builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+            
+            // Add Identity services
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddSignInManager()
+            .AddDefaultTokenProviders();
+
+            // Add authentication services
+            builder.Services.AddAuthentication(options => {
+                options.DefaultScheme = IdentityConstants.ApplicationScheme;
+                options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+            });
+
+            // Add authorization services
+            builder.Services.AddAuthorization();
+
+            // Add Identity component services
+            builder.Services.AddCascadingAuthenticationState();
+            builder.Services.AddScoped<IdentityUserAccessor>();
+            builder.Services.AddScoped<IdentityRedirectManager>();
+            builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+            
             string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
@@ -29,7 +59,12 @@ namespace DerbyDash {
             builder.Services.AddTransient<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
 
             WebApplication app = builder.Build();
-
+            app.UseStaticFiles();
+            
+            // Add authentication middleware
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment()) {
                 app.UseWebAssemblyDebugging();
@@ -38,6 +73,9 @@ namespace DerbyDash {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            // Map Identity endpoints
+            app.MapAdditionalIdentityEndpoints();
+
             }
 
             app.UseHttpsRedirection();
