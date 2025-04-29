73b4043bc46787ee7cc07fbb146731a93be3777f
felipedev418 <storm712418@gmail.com>
2025-04-29 13:08:13 +0900

    update home page

--- DerbyDash/Components/Pages/Home.razor.cs
+++ DerbyDash/Components/Pages/Home.razor.cs
@@ -5,8 +5,23 @@ namespace DerbyDash.Components.Pages {
 
     public partial class Home: ComponentBase {
         private string appName = "Derby Dash";
+        private string selectedGif = "";
 
-        public Home() {
+        private string[] gifs = {
+            "images/horse-running1.gif",
+            "images/horse-running2.gif",
+            "images/horse-running3.gif",
+            "images/horse-running4.gif",
+            "images/horse-running5.gif",
+            "images/horse-running6.gif",
+            "images/horse-running7.gif"
+        };
+
+        protected override void OnInitialized() {
+            // Select a random GIF from the array
+            Random random = new Random();
+            int index = random.Next(gifs.Length);
+            selectedGif = gifs[index];
         }
     }
 }
