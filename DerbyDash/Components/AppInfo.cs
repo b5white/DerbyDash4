cbe1d2cb10bb07f9f504b348dc2b6a8cf4b45ff0
b5white <b55white@gmail.com>
2025-05-12 18:06:50 -0500

    Removed some remnants of InteractiveServer mode Updated TODO

--- DerbyDash/Components/AppInfo.cs
+++ DerbyDash/Components/AppInfo.cs
@@ -3,7 +3,7 @@
 namespace DerbyDash.Components {
 
     public static class AppInfo {
-        static string BuildNo = "0427";
+        static string BuildNo = "0428";
  
         public static string Version = GitVersionInformation.Major + "." + GitVersionInformation.Minor + "." + GitVersionInformation.BuildMetaData + "." + BuildNo;
     }
--- DerbyDash/TODO.cs
+++ DerbyDash/TODO.cs
@@ -11,5 +11,13 @@
         // TODO Add a guard clause to prevent re-entry on adding racers and a try-finally to clear
         // DONE Default the Remeber Me to true, rename, use to save cookies or not.
         // TODO Repeat for Registration page and pass to email conf
+        // DONE FAQ page
+        // TODO Add a cookie service to handle all cookie read/write actions like save the last racer.
+        // TODO Add a feedback page so users can report errors
+        // TODO Add anchors on the FAQ page so we can link to the specific sections, like FAQ#subscription
+        // TODO Subscription page
+        // TODO Subscription service with the fake data
+        // TODO Add a link to the FAQ page from the Subscription page.
+        // TODO Move avatar to Racer
     }
 }
