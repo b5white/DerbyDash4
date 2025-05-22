9eab0c5f71a5e78687948882546a8b8163b5221f
b5white <b55white@gmail.com>
2025-05-21 23:16:58 -0500

    Tweak the about page

--- DerbyDash/Components/AppInfo.cs
+++ DerbyDash/Components/AppInfo.cs
@@ -3,7 +3,7 @@
 namespace DerbyDash.Components {
 
     public static class AppInfo {
-        static string BuildNo = "0460";
+        static string BuildNo = "0462";
  
         public static string Version = GitVersionInformation.Major + "." + GitVersionInformation.Minor + "." + GitVersionInformation.BuildMetaData + "." + BuildNo;
     }
