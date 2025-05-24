0bacac0b71c1542d636c2aba44e5235227b75d96
b5white <b55white@gmail.com>
2025-05-23 23:40:47 -0500

    Unused usings

--- DerbyDash/Data/ApplicationUser.cs
+++ DerbyDash/Data/ApplicationUser.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DerbyDash.Data {
     // Add profile data for application users by adding properties to the ApplicationUser class
