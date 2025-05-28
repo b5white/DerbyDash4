afbd53779ff09950cad09df2f708f9765e9ee6ec
felipedev418 <storm712418@gmail.com>
2025-05-29 07:32:07 +0900

    fit subscription page with profile menu

--- DerbyDash/TODO.cs
+++ DerbyDash/TODO.cs
@@ -41,8 +41,8 @@
         // TODO Add progress panel with list of steps to each of the reg pages.
         //      Change RaceTeam page to show progress panel if coming from reg,
         //      and profile menu if not.
-        // TODO Top menu needs to refresh the race count after each race.
-        // TODO Subscription should look like it's part of the profile submenu, like the others do.
+        // DONE Top menu needs to refresh the race count after each race.
+        // DONE Subscription should look like it's part of the profile submenu, like the others do.
         // TODO Don't default the active racer, except for when first adding the race team and when reading from a cookie.
         // TODO Racing should require them to have a chosen racer, and if not, redirect them to the RaceTeam page to choose one.
         // TODO Redo the menu pages to use two columns of thinner buttons
